362659bfa15e44b091a32defc0cdb0ea4b2dd170
Isaac Oluwatemilorun <akiolisa@gmail.com>
2024-03-28 13:51:47 +0100

    Update scopes

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,10 @@
 export const Scopes = [
   'https://www.googleapis.com/auth/drive.file',
+  'https://www.googleapis.com/auth/drive.metadata.readonly',
   'https://www.googleapis.com/auth/drive',
+  'https://www.googleapis.com/auth/drive.appdata',
+  'https://www.googleapis.com/auth/drive.appfolder',
+  'https://www.googleapis.com/auth/drive.resource',
 ];
 
 export const Inputs = {
